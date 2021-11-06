91412cdddd38690a05df938867ab4f9c48273996
Alicia <aliciayien360@gmail.com>
2021-11-05 23:20:09 -0700

    card comp

---
+++ comps/Card/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Cont = styled.TouchableOpacity`
+    width:204px;
+    height:269px;
+    background-color: white;
+    border-radius: 15px;
+`;
+
+const MenuImage = styled.ImageBackground`
+    height:70%;
+    width: 100%;    
+`;
+
+const ItemName = styled.Text`
+    font-size: 18px;
+    color: black;
+`;
+
+const ItemPrice = styled.Text`
+    font-size: 18px;
+    color: #484848;
+`;
+
+const PopItemIcon = styled.View`
+    height: 10%;
+    width:10%;
+`;
+
+
+
+const MenuCard = ({
+    image = require('../../assets/greenteashooter.png'),
+    itemname = "Green Tea Shot",
+    itemprice = "$5.00",
+    popularitem = require('../../assets/popularitemicon.png'),
+}) => {
+    return (
+        <Cont>
+            <MenuImage source={image} ></MenuImage>
+            <ItemName>{itemname}</ItemName>
+            <ItemPrice>{itemprice}</ItemPrice>
+            <PopItemIcon>{popularitem}</PopItemIcon>
+        </Cont>
+    );
+} 
+
+export default MenuCard;
---
+++ storybook/stories/Button/Button.stories.js
@@ -0,0 +1,31 @@
+import { action } from '@storybook/addon-actions';
+import { text } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react-native';
+import React from 'react';
+import { Text } from 'react-native';
+import Button from '.';
+import CenterView from '../CenterView';
+
+import MenuCard from '../../../comps/Card';
+
+storiesOf('Comps', module)
+  .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
+  .add('with text', () => (
+    <Button onPress={action('clicked-text')}>
+      <Text>{text('Button text', 'Hello Button')}</Text>
+    </Button>
+  ))
+
+  .add('Card', () => (
+      <MenuCard/>
+  ))
+
+  .add('Button', () => (
+    <MenuCard/>
+))
+
+  .add('with some emoji', () => (
+    <Button onPress={action('clicked-emoji')}>
+      <Text>😀 😎 👍 💯</Text>
+    </Button>
+  ));
