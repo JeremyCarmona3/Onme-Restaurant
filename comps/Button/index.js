c2dccfeb9b5fc11c046a0d5ff38b040fc2154e2e
Alicia <aliciayien360@gmail.com>
2021-11-08 10:54:46 -0800

    fixed props for buttons

---
+++ comps/Button/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const Cont = styled.TouchableOpacity`
+    width:211px;
+    height:42px;
+    background-color: #737373;
+    border-radius: 50px;
+    align-items:center;
+    justify-content: center;
+`;
+
+const ButtonText = styled.Text`
+ color: white;
+ font-size:18px;
+
+`;
+
+const Buttons = ({
+    buttonwidth = 211,
+    buttonheight=  42,
+    buttontext ="Edit Menu",
+    buttoncolor = "#737373"
+}) => {
+    return (
+        <Cont background-color={buttoncolor} width={buttonwidth} height={buttonheight}>
+            <ButtonText>{buttontext}</ButtonText>
+        </Cont>
+    );
+
+}
+
+export default Buttons;
